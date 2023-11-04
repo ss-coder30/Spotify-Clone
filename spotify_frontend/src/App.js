6b39775d752b5acb488f788bfaebcdf829599d93
SHAURYA SRIVASTAVA <30shaurya@gmail.com>
2023-11-05 00:38:45 +0530

    login part 1

--- spotify_frontend/src/App.js
+++ spotify_frontend/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import './output.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginComponent from './routes/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen">
+      <BrowserRouter>
+          {/* things inside this tag is rendered*/}
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<LoginComponent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ spotify_frontend/src/routes/Login.js
@@ -0,0 +1,16 @@
+import { Icon } from '@iconify/react';
+
+const LoginComponent = () => {
+    return (
+        <div className="w-full h-full flex flex-col items-center">
+            <div className="logo p-6 border-b border-solid border-gray-200 w-full flex justify-center">
+                <Icon icon="logos:spotify" width={150} />   
+            </div>
+            <div className="inputRegion">
+                
+            </div>
+        </div>
+    )
+};
+
+export default LoginComponent;  
